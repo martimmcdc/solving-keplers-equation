e427930f8f4250f962d52bb71e199172ba1333c8
João Faria <joao.faria@astro.up.pt>
2021-06-01 15:22:58 +0100

    iter_counter False by default

--- iterative_solver.py
+++ iterative_solver.py
@@ -2,14 +2,16 @@
 from numpy import sin
 
 ### Método iterativo
-def iterative_solver(e,M,epsilon=1e-9,iter_counter=True):
-	x0 = M                  # initial guess
-	x = M + e*sin(x0)       # primeira iteração
-	counter = 1
-	while abs(x0-x)>x*epsilon: # defini o erro como erro relativo entre iterações sucessivas
-		x0 = x                 # atualizar último valor calculado
-		x = M + e*sin(x0)   # calcular seguinte
-		counter += 1
-	if iter_counter==True:
-		return x,counter
-	return x
+def iterative_solver(e, M, epsilon=1e-9, iter_counter=False):
+    x0 = M                  # initial guess
+    x = M + e*sin(x0)       # primeira iteração
+    counter = 1
+    while abs(x0-x)>x*epsilon: # defini o erro como erro relativo entre iterações sucessivas
+        x0 = x                 # atualizar último valor calculado
+        x = M + e*sin(x0)   # calcular seguinte
+        counter += 1
+
+    if iter_counter:
+        return x,counter
+
+    return x
