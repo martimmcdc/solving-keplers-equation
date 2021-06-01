2a4a0918b2ba4fa05c645af5dd8ba02e2a4db26a
João Faria <joao.faria@astro.up.pt>
2021-06-01 15:28:33 +0100

    tiny changes

--- newton_solver.py
+++ newton_solver.py
@@ -2,14 +2,16 @@
 from numpy import sin,cos
 
 ### Método de Newton-Raphson
-def newton_solver(e,M,epsilon=1e-9,iter_counter=False):
-	x0 = M                                        # initial guess
-	x = x0 - (x0 - e*sin(x0) - M)/(1 - e*cos(x0)) # primeira iteração
-	counter = 1
-	while abs(x0-x)>x*epsilon: # defini o erro como erro relativo entre iterações sucessivas
-		x0 = x                 # atualizar último valor calculado
-		x = x0 - (x0 - e*sin(x0) - M)/(1 - e*cos(x0)) # calcular seguinte
-		counter += 1
-	if iter_counter==True:
-		return x,counter
-	return x
+def newton_solver(e, M, epsilon=1e-9, iter_counter=False):
+    x0 = M                                        # initial guess
+    x = x0 - (x0 - e*sin(x0) - M)/(1 - e*cos(x0)) # primeira iteração
+    counter = 1
+    while abs(x0-x)>x*epsilon: # defini o erro como erro relativo entre iterações sucessivas
+        x0 = x                 # atualizar último valor calculado
+        x = x0 - (x0 - e*sin(x0) - M)/(1 - e*cos(x0)) # calcular seguinte
+        counter += 1
+
+    if iter_counter:
+        return x, counter
+
+    return x
