57b936809d668d398aa0902f4081a90662f675d2
martimmcdc <martimdams@gmail.com>
2021-06-13 14:42:27 +0100

    Update test1.py
    Added comments, changed timing method from time() to timeit().
    Still runs over every value with M as scalar

--- test1.py
+++ test1.py
@@ -1,46 +1,81 @@
-### imports
+# Comparison of Newton-Raphson vs Iterative (direct substitution) methods
+
+# This script generates heatmaps of the logarithms of runtimes and iteration numbers...
+# ...of each method for a grid of 200x200 values of eccentricity and mean anomaly.
+
+# The root is considered to be found when the relative error between successive iterations...
+# ...reaches a value of epsilon=1e-9, that is |E(i)-E(i+1)|/E(i+1)<epsilon
+
+# The results of this test are printed to the console and can be seen to be best for...
+# ...the Newton-Raphson method, which converges faster for most values
+
+
+### imports ###
 import numpy as np
 import matplotlib.pyplot as plt
-from time import time
+from timeit import timeit
 from iterative_solver import iterative_solver
 from newton_solver import newton_solver
 
-N = 300                    # number of grid points for e and M
+
+### Grid values ###
+N = 200                    # number of grid points for e and M
 M = np.linspace(0,np.pi,N) # mean anomaly values in [0,pi]
 e = np.arange(0,1,1/N)     # eccentricity values in [0,1)
 
-### Cálculo
-zeros = np.zeros([2,len(e),len(M)],float)  # array to store the roots
-iterations = np.zeros(zeros.shape,float)   # array to store the number of iterations
-times = np.zeros(zeros.shape,float)        # array to store the run time
+
+### Calculation ###
+
+Nt = 10 # number of calculation runtimes to average over
+iterations = np.zeros([2,len(e),len(M)],float) # array to store the number of iterations
+times = np.zeros([2,len(e),len(M)],float)      # array to store the run time
 methods = [newton_solver,iterative_solver] # methods to compare
+
 for i in range(2):
 	method = methods[i]
+	name = method.__name__                             # method name to feed timeit function
+	set_up = '''from {} import {}'''.format(name,name) # setup to be subtracted from runtime estimate
 	for j in range(len(M)):
 		Mj = M[j]
 		for k in range(len(e)):
-			start = time()
-			zeros[i,j,k],iterations[i,j,k] = method(e[k],Mj,epsilon=1e-9,iter_counter=True) 
-			times[i,j,k] = time() - start
+			ek = e[k]
+			statement = '''{}({},{},epsilon=1e-9,iter_counter=True)'''.format(name,ek,Mj) # statement to be timed
+			times[i,j,k] = timeit(stmt=statement,setup=set_up,number=Nt)                  # runtime for 10 calculations
+			iterations[i,j,k] = method(ek,Mj,epsilon=1e-9,iter_counter=True)[1] # values of iteration number
+times /= Nt # average
 
-### Gráfico
 
+### Plot ###
+
+# figure
+fig,axs = plt.subplots(2,3,
+	gridspec_kw={'width_ratios':[1,1,0.05],
+	             'hspace':0.05,
+	             'wspace':0.05})
+
+# relative values for colorbar
+min_val1 = np.min(np.log10(times))
+max_val1 = np.max(np.log10(times))
+min_val2 = np.min(np.log10(iterations))
+max_val2 = np.max(np.log10(iterations))
+
+# heatmaps
+ax1 = axs[0,0].imshow(np.log10(times[0,:,:]),vmin=min_val1,vmax=max_val1,cmap=plt.cm.hot,origin='lower')
+ax2 = axs[0,1].imshow(np.log10(times[1,:,:]),vmin=min_val1,vmax=max_val1,cmap=plt.cm.hot,origin='lower')
+ax3 = axs[1,0].imshow(np.log10(iterations[0,:,:]),vmin=min_val2,vmax=max_val2,origin='lower')
+ax4 = axs[1,1].imshow(np.log10(iterations[1,:,:]),vmin=min_val2,vmax=max_val2,origin='lower')
+
+# axes for colorbars
+ax5 = axs[0,2]
+ax6 = axs[1,2]
+fig.colorbar(ax2,cax=ax5,label='$\\log(\Delta t)$ , $\Delta t$ in seconds')
+fig.colorbar(ax4,cax=ax6,label='\n$\\log(n)$ , $n = $number of iterations')
+
+# plot labeling
 eticks = (e[::N//10]*N)[1:]
 elabels= np.round(e[::N//10],1)[1:]
 Mticks = np.linspace(0,N,5)
 Mlabels= ['0','$\\frac{\pi}{4}$','$\\frac{\pi}{2}$','$\\frac{3\pi}{4}$','$\\pi$']
-
-fig,axs = plt.subplots(2,3,figsize=(6,6),
-	gridspec_kw={'width_ratios':[1,1,0.05],
-	             'hspace':0.05,
-	             'wspace':0.05})
-ax1 = axs[0,0].imshow(np.log(times[0,:,:]),cmap=plt.cm.hot,origin='lower')
-ax2 = axs[0,1].imshow(np.log(times[1,:,:]),cmap=plt.cm.hot,origin='lower')
-ax3 = axs[1,0].imshow(np.log(iterations[0,:,:]),origin='lower')
-ax4 = axs[1,1].imshow(np.log(iterations[1,:,:]),origin='lower')
-ax5 = axs[0,2]
-ax6 = axs[1,2]
-
 axs[1,0].set_xticks(Mticks)
 axs[1,0].set_xticklabels(Mlabels)
 axs[1,1].set_xticks(Mticks)
@@ -53,14 +88,30 @@ axs[0,0].tick_params(direction='in',labelbottom=False)
 axs[0,1].tick_params(direction='in',labelbottom=False,labelleft=False)
 axs[1,0].tick_params(direction='in')
 axs[1,1].tick_params(direction='in',labelleft=False)
-
 axs[1,0].set_xlabel('$M$')
 axs[1,1].set_xlabel('$M$')
 axs[0,0].set_ylabel('$e$')
 axs[1,0].set_ylabel('$e$')
-
 axs[0,0].set_title('Newton-Raphson')
 axs[0,1].set_title('Iterative')
-fig.colorbar(ax2,cax=ax5,label='$\\log(\Delta t)$ , $\Delta t$ in seconds')
-fig.colorbar(ax4,cax=ax6,label='\n$\\log(n)$ , $n = $number of iterations')
+
+# save and show
+plt.savefig('test1_out.pdf')
 plt.show()
+
+
+### Print statistics ###
+phrases = np.array(['Mean calculation time: ','Standard deviation: ','Min. calculation time: ','Max. calculation time: '])
+phrases[1] = ' '*(len(phrases[0])-len(phrases[1]))+phrases[1]
+[mean1,mean2] = np.mean(times,axis=(1,2))
+[std1,std2] = np.std(times,axis=(1,2))
+[min1,min2] = np.min(times,axis=(1,2))
+[max1,max2] = np.max(times,axis=(1,2))
+
+print('\n')
+print(' '*len(phrases[0]) + 'My implementation  |  Original')
+print(phrases[0] + '   {:.3e}    |  {:.3e}'.format(mean1,mean2))
+print(phrases[1] + '   {:.3e}    |  {:.3e}'.format(std1,std2))
+print(phrases[2] + '   {:.3e}    |  {:.3e}'.format(min1,min2))
+print(phrases[3] + '   {:.3e}    |  {:.3e}'.format(max1,max2))
+print('\n')
