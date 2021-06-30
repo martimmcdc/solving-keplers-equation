cf0442ea714dc967d41829361fb3effa82dc899d
martimmcdc <martimdams@gmail.com>
2021-06-30 18:42:03 +0100

    Create murison_solver.py
    Script with Murison's solver. Does not work for high eccentricity at low mean anomaly (either a mistake in the code or a problem with the algorithm itself).

---
+++ murison_solver.py
@@ -0,0 +1,88 @@
+"""
+This script is an implementation of Murison's method for solving Kepler's equation.
+The key part of this method is the initial guess.
+The iterations stop after a precision epsilon on the functions value (which we wish to be f(x)=0) is reached.
+
+This measure of error is discussed on a pdf file available on:
+https://github.com/martimmcdc/solving-keplers-equation
+
+In order to minimize calculations but still properly evaluate the precision of each iteration,
+the error is considered to be that of the previous iteration, so that,
+when the desired precision is reached, the algorithm runs one additional time.
+"""
+
+### imports
+import numpy as np
+
+def KeplerStart3(e,M):
+    t34 = e*e
+    t35 = e*t34
+    t33 = np.cos(M)
+    return M+(-0.5*t35 + e + (t34+1.5*t33*t35)*t33)*np.sin(M)
+
+def eps3(e,M,x):
+    t1 = np.cos(x)
+    t2 = -1+e*t1
+    t3 = np.sin(x)
+    t4 = e*t3
+    t5 = -x+t4+M
+    t6 = t5/(0.5*t5*t4/t2+t2)
+    return t5/((0.5*t3 - 1/6*t1*t6)*e*t6+t2)
+
+def murison_solver(e,M,epsilon=1e-9,iter_counter=False):
+    Mnorm = M%(2*np.pi)
+    E = KeplerStart3(e,Mnorm)
+    filt = np.ones(len(M),bool)
+    counter = 0
+    while filt.any():
+        E[filt] -= eps3(e,Mnorm[filt],E[filt].copy())
+        filt[filt] = (E[filt] - e*np.sin(E[filt]) - M[filt])>epsilon
+        counter += 1
+    if iter_counter:
+        return E,counter
+    else:
+        return E
+
+
+# Test function
+if __name__=="__main__":
+    
+    import matplotlib.pyplot as plt,time
+
+    # Parameters
+    N = 1000000
+    e = 0.5
+
+    print("\n##### PARAMETERS #####")
+    print("# len(M) = %d"%N)
+    print("# e = %.2f"%e)
+    #print("# Iterations: %d"%N_it)
+    print("######################")
+
+    # Create ell array from E
+    E_true = (np.pi*(np.arange(N)+0.5))/N
+    M_input = E_true - e*np.sin(E_true)
+
+    # Time the function
+    init = time.time()
+    E_out,iters = murison_solver(e,M_input,iter_counter=True)
+    runtime = time.time()-init
+    print("\nEstimation complete after %.1f millseconds, achieving mean error %.2e."%(runtime*1000.,np.mean(np.abs(E_out-E_true))))
+    print(iters,'iterations\n')
+
+
+    # Plot values calculated and compare with pre-calculated grid
+    sympy_grid = np.loadtxt('sympy_200x200grid.txt')[::30,:]
+    M = np.linspace(0,np.pi,200)
+    e = np.arange(0,1,1/200)[::30]
+    for power in range(6,16,3):
+        for i in range(len(e)):
+            vals = murison_solver(e[i],M,epsilon=10**(-power))
+            sympy_vals = sympy_grid[i,:]
+            plt.plot(M,vals-sympy_vals,label='e = {}'.format(e[i]))
+        plt.xlabel('$M$')
+        plt.ylabel('$\Delta E$')
+        plt.title('$\\epsilon = 10^{}$'.format('{'+str(-power)+'}'))
+        plt.legend()
+        plt.grid()
+        plt.show()
