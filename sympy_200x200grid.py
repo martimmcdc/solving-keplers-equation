b8194d58f4d990e4d372abf28aa8fae3e15b26f4
martimmcdc <martimdams@gmail.com>
2021-06-30 15:16:22 +0100

    Minor changes

--- sympy_200x200grid.py
+++ sympy_200x200grid.py
@@ -1,16 +1,18 @@
-# Grid generator to compare values obtained by other methods
+"""
+Grid generator to compare values obtained by other methods
+"""
 
 ### imports
 from sympy_solver import sympy_solver
-from numpy import pi,arange,linspace,empty,savetxt,vectorize
+import numpy as np
 
 N = 200
-array = empty([N,N],float)
-M = linspace(0,pi,N)
-e = arange(0,1,1/N)
+array = np.empty([N,N],float)
+M = np.linspace(0,pi,N)
+e = np.arange(0,1,1/N)
 
 for i in range(N):
 	ei = e[i]
-	array[i,:] = vectorize(sympy_solver)(ei,M)
+	array[i,:] = np.vectorize(sympy_solver)(ei,M)
 
-savetxt('sympy_200x200grid.txt',array)
+np.savetxt('sympy_200x200grid.txt',array)
