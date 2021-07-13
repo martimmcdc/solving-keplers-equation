4e2bda53d52b04a3f69356f49ec0cf77ff809309
martimmcdc <martimdams@gmail.com>
2021-07-13 11:19:27 +0100

    Prepare data for test4.py
    test4_prep.py generates grids of runtimes and found roots for each method and saves them to the solver_grids folder.

---
+++ test4_prep.py
@@ -0,0 +1,44 @@
+"""
+This script generates 200x200 grids in (e,M)-space containing for each method the calculated root and runtime.
+The values are saved as .txt files containing the arrays in the solver_grids/ folder.
+They are accessed and used by test4.py .
+"""
+
+### imports ###
+import numpy as np
+from timeit import timeit
+from iterative_solver import iterative_solver
+from newton_solver import newton_solver
+from goat_herd_solver import goat_herd_solver
+from nijenhuis_solver import nijenhuis_solver
+from danby_solver import danby_solver
+from cordic_solver import cordic_solver
+from murison_solver import murison_solver
+
+methods = [newton_solver,iterative_solver,goat_herd_solver,nijenhuis_solver,danby_solver,cordic_solver,murison_solver]
+
+### Grid values ###
+N = 200                    # number of grid points for e and M
+M = np.linspace(0,np.pi,N) # mean anomaly values in [0,pi]
+e = np.arange(0,1,1/N)     # eccentricity values in [0,1)
+
+### Calculation ###
+methods = [newton_solver,iterative_solver,goat_herd_solver,nijenhuis_solver,danby_solver,cordic_solver,murison_solver] # functions
+Nt = 5
+zeros = np.zeros([len(methods),len(e),len(M)],float)  # array to store the roots
+times = np.zeros(zeros.shape,float)        # array to store the run time
+M_array = M # M input as array without 0 and pi values
+for i in range(len(methods)):
+	method = methods[i]
+	name = method.__name__ # method name to feed timeit function
+	set_up = "import numpy as np;from {} import {}".format(name,name) # setup to be subtracted from runtime estimate
+	for k in range(1,len(M_array)-1):
+		Mk = np.array([M_array[k]])
+		for j in range(1,len(e)-1):
+			ej = e[j]
+			statement = "{}({},np.{})".format(name,ej,repr(Mk)) # statement to be timed
+			times[i,j,k] = timeit(stmt=statement,setup=set_up,number=Nt) # runtime for 10 calculations
+			zeros[i,j,k] = method(ej,Mk) # values of iteration number
+	fname = 'solver_grids/'+name.strip('solver')+'200x200grid_'
+	np.savetxt(fname+'runtime.txt',times[i,:,:])
+	np.savetxt(fname+'zeros.txt',zeros[i,:,:])
